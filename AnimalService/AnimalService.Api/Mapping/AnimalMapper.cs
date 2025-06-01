a308e630a0cf3e05b3ba19cee57ea10461a0d8da
william.calissendorff.brodd@gmail.com <william.calissendorff.brodd@gmail.com>
2025-06-01 10:57:51 +0200

    Added mapper from dto to domain. With overload incase AnimalId is sent from client, as in an Update/PUT request

--- AnimalService/AnimalService.Api/Mapping/AnimalMapper.cs
+++ AnimalService/AnimalService.Api/Mapping/AnimalMapper.cs
@@ -1,5 +1,7 @@
 ﻿using AnimalService.Api.Models;
 using AnimalService.Domain.Classes;
+using AnimalService.Domain.Enums;
+using SharedKernel.Identifiers;
 
 namespace AnimalService.Api.Mapping
 {
@@ -8,9 +10,26 @@ namespace AnimalService.Api.Mapping
         public static AnimalDto MapToDto(Animal animal) => new()
         {
             Id = animal.Id.Value,
-            Type = animal.Type.ToString(),
+            Type = animal.Type,
             Name = animal.Name,
             AgeInYears = animal.AgeInYears
         };
+
+        public static Animal MapToDomain(AnimalDto dto) => new()
+        {
+            Id = new AnimalId(dto.Id),
+            Type = dto.Type,
+            Name = dto.Name,
+            AgeInYears = dto.AgeInYears
+        };
+
+        public static Animal MapToDomain(AnimalDto dto, AnimalId id) => new()
+        {
+            Id = id,
+            Type = dto.Type,
+            Name = dto.Name,
+            AgeInYears = dto.AgeInYears
+        };
+
     }
 }
