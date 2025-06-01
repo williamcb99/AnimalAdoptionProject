5b1ce40e994c3fb88106d672fb2a001447a9f4d3
william.calissendorff.brodd@gmail.com <william.calissendorff.brodd@gmail.com>
2025-06-01 11:25:24 +0200

    Added endpoint for AddAnimal(). It uses Model validation from its DataAnnotations to ensure body in request is correctly filled.

--- AnimalService/AnimalService.Api/Controllers/AnimalsController.cs
+++ AnimalService/AnimalService.Api/Controllers/AnimalsController.cs
@@ -3,6 +3,7 @@ using AnimalService.Api.Models;
 using AnimalService.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using SharedKernel.Identifiers;
+using System.Reflection.Metadata.Ecma335;
 
 namespace AnimalService.Api.Controllers
 {
@@ -18,11 +19,11 @@ namespace AnimalService.Api.Controllers
             _animalRepository = animalRepository;
         }
 
+        [HttpGet("GetAllAnimals")]
         [EndpointDescription("Get all animals")]
         [EndpointSummary("Get all animals")]
         [ProducesResponseType(typeof(List<AnimalDto>), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(string), StatusCodes.Status500InternalServerError)]
-        [HttpGet("GetAllAnimals")]
         public async Task<IActionResult> GetAllAnimals()
         {
             var animals = (await _animalRepository.GetAllAsync())?.ToList();
@@ -34,11 +35,11 @@ namespace AnimalService.Api.Controllers
             return Ok(animalDtos);
         }
 
+        [HttpGet("GetAnimalById/{id:guid}")]
         [EndpointDescription("Get animal by ID")]
         [EndpointSummary("Get animal by ID")]
         [ProducesResponseType(typeof(AnimalDto), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
-        [HttpGet("GetAnimalById/{id:guid}")]
         public async Task<IActionResult> GetAnimalById([FromRoute] Guid id)
         {
             var animalId = new AnimalId(id);
@@ -67,6 +68,24 @@ namespace AnimalService.Api.Controllers
             {
                 return NotFound($"Animal with ID {id} not found.");
             }
+        }
+
+        [HttpPost("AddAnimal")]
+        [EndpointDescription("Create animal")]
+        [EndpointSummary("Create animal")]
+        [ProducesResponseType(typeof(AnimalDto), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> AddAnimal([FromBody] CreateAnimalDto newAnimal)
+        {
+            if (newAnimal == null)
+                return BadRequest("Request body cannot be empty.");
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);            
+            var animalId = new AnimalId(Guid.NewGuid());
+            var animal = AnimalMapper.MapToDomain(newAnimal, animalId);
+            await _animalRepository.AddAsync(animal);
+            var createdAnimalDto = AnimalMapper.MapToDto(animal);
+            return CreatedAtAction(nameof(GetAnimalById), new { id = createdAnimalDto.Id }, createdAnimalDto);
         }
     }
 }
